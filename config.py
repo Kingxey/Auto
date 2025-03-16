99b95772b9da4126427d626c89f44920b53989d0
Noob Dev <kingceylegend@gmail.com>
2025-03-17 04:16:42 +1100

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@
 import os
 from dotenv import load_dotenv
 
-# Charger les variables d'environnement
+# poue le loaddotenv que m'a appris hyosh 
 load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# Dictionnaire pour stocker l'état du bot
+# Pour stockage de l'état du bot
 BOT_STATE = {
     "channel_id": None,
     "messages": [],
